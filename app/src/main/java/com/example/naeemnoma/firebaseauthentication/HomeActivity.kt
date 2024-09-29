ae1d2389313b66be5cf9ab1ff821ee00b303b773
naeemnoman <naeemnoman0419@gmail.com>
2024-09-29 09:26:28 +0600

    done

--- app/src/main/java/com/example/naeemnoma/firebaseauthentication/HomeActivity.kt
+++ app/src/main/java/com/example/naeemnoma/firebaseauthentication/HomeActivity.kt
@@ -5,16 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.naeemnoma.firebaseauthentication.ViewModel.AuthViewModels
+import com.example.naeemnoma.firebaseauthentication.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var viewModels: AuthViewModels
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModels = ViewModelProvider(this).get()
     }
 }
--- app/src/main/java/com/example/naeemnoma/firebaseauthentication/RegistrationActivity.kt
+++ app/src/main/java/com/example/naeemnoma/firebaseauthentication/RegistrationActivity.kt
@@ -1,26 +1,53 @@
 package com.example.naeemnoma.firebaseauthentication
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.AndroidViewModel
-import androidx.viewbinding.ViewBinding
+import androidx.lifecycle.ViewModelProvider
 import com.example.naeemnoma.firebaseauthentication.ViewModel.AuthViewModels
+import com.example.naeemnoma.firebaseauthentication.databinding.ActivityRegestrationBinding
 
-class RegestrationActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
 
 
     private lateinit var viewModel: AuthViewModels
-    private lateinit var binding: RegestrationActivity
+    private lateinit var binding: ActivityRegestrationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        binding = RegestrationActivity.inflate(layoutInflater)
-
+        binding = ActivityRegestrationBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(AuthViewModels::class.java)
+        binding.registerBtn.setOnClickListener {
+
+            val email = binding.emailEt.text.toString()
+            val password = binding.emailPasswordEt.text.toString()
+            val confirmPassword = binding.ConfrimPasswordEt.text.toString()
+            if(email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                Toast.makeText(this@RegistrationActivity, "Register Success", Toast.LENGTH_SHORT).show()
+
+                viewModel.singUp(email, password)
+
+            }else if (!password.equals(confirmPassword)) {
+                Toast.makeText(this@RegistrationActivity, "Password not match", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                viewModel.singUp(email, password).observe(this,{result->
+                    Toast.makeText(this@RegistrationActivity, result, Toast.LENGTH_SHORT).show()
+
+                    if(result.equals("Singup Success")){
+                        startActivity(Intent(this@RegistrationActivity, HomeActivity::class.java))
+                    }
+            })
+        }
+            binding.AlreadyHaveAnAccountTxt.setOnClickListener {
+                startActivity(Intent(this@RegistrationActivity, MainActivity::class.java))
+            }
+        }
+
+
     }
 }
